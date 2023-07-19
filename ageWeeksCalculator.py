8a4a79a331615e1f880d6f74f826d564df723a31
Radhi hudijan <radhi@digifit.eu>
2023-07-19 18:31:05 +0200

    age calculator

---
+++ ageWeeksCalculator.py
@@ -0,0 +1,9 @@
+print('Hello Friend')
+
+age = input('what is your age now ?\n')
+weeksIn90Years = 90*12*4
+
+currentAgeInWeeks = int(age) * 12 * 4
+weeksLeft = weeksIn90Years-currentAgeInWeeks
+
+print(f'you have {weeksLeft} left to rach 90 years')
